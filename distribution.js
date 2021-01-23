a466e6f4daee7506ad80ec996308a97cecb500ee
polat-poyraz <poyrazpolat647@gmail.com>
2021-01-23 03:23:03 +0300

    distribution path name bug fixed.

--- distribution.js
+++ distribution.js
@@ -1,5 +1,5 @@
-import { Vanille } from './src/Vanille.js'
-import { $componentUse } from './src/component-use.js'
+import { Vanille } from './src/vanille.js'
+import { $componentUse } from './src/user-functions/component.use.js'
 import { getTemplate } from './src/user-functions/get.template.js'
 import { tools } from './src/tools.js'
 
