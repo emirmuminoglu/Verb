4bb715912518da820f6c45b0051898eb391c09dc
polat-poyraz <poyrazpolat647@gmail.com>
2021-01-23 03:13:44 +0300

    createKey: remove mark

--- src/create.key.js
+++ src/create.key.js
@@ -2,7 +2,7 @@ export const createKey = () => {
     let defaultKey = 'data-l-'
     
     for (let i = 0; i < 3; i++) {
-        const alphabet = 'abcde!f#ghi|j?klm$no%prsjt&u=vy#z'
+        const alphabet = 'abcdefghijklmnoprsjtuvyz'
         const randomNum = Math.floor(Math.random() * 10)
         const randomAph = Math.floor(Math.random() * alphabet.length)
         defaultKey += randomNum + alphabet[randomAph]
