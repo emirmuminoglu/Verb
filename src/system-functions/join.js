6e45ab77f33642767bedacccfc6dd7866347367f
polat-poyraz <poyrazpolat647@gmail.com>
2021-01-24 07:28:54 +0300

    The usage of arguments in HTML has been updated. JavaScript has been made available

--- src/system-functions/join.js
+++ src/system-functions/join.js
@@ -1,5 +1,3 @@
-import { control } from "./error.js"
-
 const changeHandler = (value, changes, variableName) => {
     if (changes[variableName]) {
         return changes[variableName](value)
@@ -9,34 +7,9 @@ const changeHandler = (value, changes, variableName) => {
 }
 
 export const join = (state, changes, variableName) => {
-    let trueValue
-    let changeValue
-
-    if (variableName.includes(".")) {
-        let routers = variableName.split("."),
-        rootMode = true,
-        output = ""
-
-        routers.map(route => {
-            if (rootMode) {
-                output = state[route]
-
-                rootMode = false
-            } else {
-                output = output[route]
-            }
-
-            control(`${output}`).isNot({ value: "undefined" }).err(`A value named ${variableName} was not found in state. Check the values ​​you use in HTML`)
-        })
-
-        trueValue = output
-        changeValue = changeHandler(output, changes, variableName)
-    } else {
-        control(`${state[variableName]}`).isNot({ value: "undefined" }).err(`A value named ${variableName} was not found in state. Check the values ​​you use in HTML`)
-        trueValue = state[variableName]
-        changeValue = changeHandler(trueValue, changes, variableName)
-    }
-
+    const trueValue = eval(variableName),
+    changeValue = changeHandler(trueValue, changes, variableName)
+    
     return {
         trueValue: typeof trueValue === "object" ? JSON.stringify(trueValue) : trueValue,
         changeValue: typeof changeValue === "object" ? JSON.stringify(changeValue) : changeValue
