f63f05fa2124e54c0a25b853a73c1e2aee0f8266
polat-poyraz <poyrazpolat647@gmail.com>
2021-02-12 19:58:45 +0300

    test hook added

--- src/particles/hooks/distribution.js
+++ src/particles/hooks/distribution.js
@@ -1,3 +1,4 @@
 import { Node } from "./node.js"
+import { test } from './test.js'
 
-export const hooks = { Node }
+export const hooks = { Node, test }
---
+++ src/particles/hooks/test.js
@@ -0,0 +1,24 @@
+export const test = (title, { max, repeat }, _function) =>{
+    return new Promise(async (resolve, reject) => {
+        let timer = 0
+        const timerInterval = setInterval(() => timer++, 0),
+            returns = []
+
+        for (let i = 0; i < repeat; i++) {
+            returns.push(await _function())
+        }
+
+        clearInterval(timerInterval)
+
+        const result = (`@ ${title}
+time: ${timer}ms
+max time: ${max}ms
+test result: ${timer > max ? 'failed' : 'success'}
+test function returns: ${JSON.stringify(returns)}
+`)
+
+        if (timer <= max) {
+            resolve(result)
+        } else reject(result)
+    })
+}
