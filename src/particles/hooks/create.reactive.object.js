a810ca666ff0f9da67dbc9c059afcb078086054d
polat-poyraz <poyrazpolat647@gmail.com>
2021-02-26 00:03:01 +0300

    new hook: createReactiveObject

---
+++ src/particles/hooks/create.reactive.object.js
@@ -0,0 +1,24 @@
+export const createReactiveObject = (obj, _function = () => { }) => {
+    const map = new WeakMap()
+
+    for (const key in obj) {
+        if (typeof obj[key] === 'object') {
+            createReactiveObject(obj[key])
+        } else if (!key.includes('@')) {
+            map.set(obj, { ...obj })
+
+            Object.defineProperty(obj, key, {
+                get() {
+                    return map.get(obj)[key]
+                },
+                set(value) {
+                    map.get(obj)[key] = value
+
+                    map.get(obj)['@' + key]()
+                }
+            })
+        }
+    }
+
+    return obj
+}
--- src/particles/hooks/distribution.js
+++ src/particles/hooks/distribution.js
@@ -1,4 +1,5 @@
 import { Node } from "./node.js"
 import { test } from './test.js'
+import { createReactiveObject } from './create.reactive.object.js'
 
-export const hooks = { Node, test }
+export const hooks = { Node, test, createReactiveObject }
