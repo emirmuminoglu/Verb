ec229c04000fef4e51ab829a1da81302e56fc44e
= <=>
2021-03-04 13:08:59 +0300

    router and store removed

--- distribution.js
+++ distribution.js
@@ -1,14 +1,10 @@
 import { Verb } from "./src/verb.js"
-import { Router } from "./src/router/router.js"
 import { hooks } from "./src/particles/hooks/distribution.js"
 import { tools } from "./src/tools.js"
 import { JSS } from './src/system/JSS.js'
-import { Vex } from "./src/store/store.js"
 
 export {
     Verb,
-    Router,
-    Vex,
     JSS,
     hooks,
     tools
